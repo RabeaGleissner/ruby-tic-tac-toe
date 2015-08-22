fc6406a589a77c4dfe51c9fc0147d5cfa5cb7572
Rabea Gleissner <rabeagleissner@gmail.com>
2015-08-22 10:42:52 +0100

    places a mark in a given position if the position is free

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,27 @@
 class Board
 
-  def create_board
-    (1..9).to_a
+  attr_reader :cells
+
+  def initialize
+    @cells = (1..9).to_a
   end
+
+  def place_mark(position, mark)
+    if position_empty?(position)
+      @cells[position-1] = mark
+      @cells
+    else
+      'Sorry, this position is already occupied!'
+    end
+  end
+
+  def position_empty?(position)
+    if @cells[position-1].kind_of? Integer
+      true
+    else
+      false
+    end
+  end
+
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,8 +3,25 @@ require 'spec_helper'
 
 
 describe Board do
+  let(:board) {Board.new}
+
   it 'creates an empty board' do 
-    board = Board.new
-    expect(board.create_board).to eq([1,2,3,4,5,6,7,8,9])
+    expect(board.cells).to eq([1,2,3,4,5,6,7,8,9])
+  end
+
+  it 'given a mark and position, it places it on the board' do
+    expect(board.place_mark(1, 'x')).to eq(['x',2,3,4,5,6,7,8,9])
+    expect(board.place_mark(3, 'o')). to eq(['x',2,'o',4,5,6,7,8,9])
+  end
+
+  it 'checks if a position is empty' do
+    expect(board.position_empty?(2)).to eq true
+    board.place_mark(2, 'x')
+    expect(board.position_empty?(2)).to eq false
+  end
+
+  it 'shows an error message if it tries to place a mark in an occpuied position' do
+    board.place_mark(2, 'x')
+    expect(board.place_mark(2, 'o')). to eq('Sorry, this position is already occupied!')
   end
 end
