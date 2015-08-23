55fde0e7595905486433ab491a61c98c43051b73
Rabea Gleissner <rabeagleissner@gmail.com>
2015-08-23 09:48:40 +0100

    adds User with name and mark

---
+++ lib/user.rb
@@ -0,0 +1,8 @@
+class User
+  attr_reader :name, :mark
+
+  def initialize(name, mark)
+    @name = name
+    @mark = mark
+  end
+end
---
+++ spec/user_spec.rb
@@ -0,0 +1,15 @@
+require 'user'
+require 'spec_helper'
+require 'pry-byebug'
+
+describe User do
+  let(:user) {User.new('Joe', 'x')}
+
+  it 'has a name' do
+    expect(user.name).to eq 'Joe'
+  end
+
+  it 'has a mark' do
+    expect(user.mark).to eq 'x'
+  end
+end
