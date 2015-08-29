d88ac0047ef8ddb377f203feea6e45166b0ce8b2
Rabea Gleissner <rabeagleissner@gmail.com>
2015-08-29 19:51:27 +0100

    it returns the correct move if the next move is a winning move

--- lib/perfect_player.rb
+++ lib/perfect_player.rb
@@ -1,4 +1,7 @@
 class PerfectPlayer
+
+  WIN_ARRAYS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
   attr_reader :mark, :board
 
   def initialize(mark, board)
@@ -16,65 +19,39 @@ class PerfectPlayer
     end
   end
 
-  def method_name
-    
-  end
-
   def mini_max
     game_state = @board.cells.clone
-    copy_of_available_positions = @board.available_positions
+    copy_of_available_positions = @board.available_positions.clone
     moves_with_rating = {}
     moves_with_rating = {}
     free_positions_hash = {}
 
-    #turning free positions into a hash of available moves with a placeholder for rating
+    # turning free positions into a hash of available moves with a placeholder for rating
     copy_of_available_positions.each_with_index { |index, move| moves_with_rating[index] = move}
-      # moves_with_rating ={5=>0, 6=>1, 7=>2, 8=>3}
     moves_with_rating.each { |key, value| moves_with_rating[key] = 'rating' }
-      # moves_with_rating = {5=>"rating", 6=>"rating", 7=>"rating", 8=>"rating"}
     
     copy_of_available_positions.each_with_index { |index, move| free_positions_hash[index] = move}
-      # free_positions_hash = {5=>0, 6=>1, 7=>2, 8=>3}
 
-    #assigning a duplicate of the free_positions_hash at the start of the iteration
+    # assigning a duplicate of the free_positions_hash at the start of the iteration
     free_positions_hash_dup = free_positions_hash.clone
 
     moves_with_rating.each do |move, rating|
-      #{5=>0,6=>1,7=>2,8=>3}
+
       mark = @mark
 
       free_positions_hash_dup.each do |index, value| 
         free_positions_hash_dup[index] = mark
+        game_state(free_positions_hash_dup, game_state)
         mark = switch_mark(mark)
       end
-      # free_positions_hash_dup =  {5=>"x", 6=>"o", 7=>"x", 8=>"o"}
+
       # for next iteration need to start with placing 'x' at key 6
-      
-     # placing the marks in the correct index positions of the game state array
-     free_positions_hash_dup.each do |index, mark|
-      #["x", "x", "o", "x", "o", 5, 6, 7, 8]
-      game_state[index] = mark 
-     end
-     #game_state1 = ["x", "x", "o", 
-                    # "x", "o", "x",
-                    # "o", "x", "o"] should be -10
-     #game_state2 = ["x", "x", "o",
-                   # "x", "o", "o", 
-                   # "x", "o", "x"] should be +10
-     #game_state3 = ["x", "x", "o",
-                   # "x", "o", "x", 
-                   # "o", "x", "o"] should be -10
-      #game_state4 = ["x", "x", "o",
-                    # "x", "o", "o", 
-                    # "x", "o", "x"] should be +10
-      opponent_mark = switch_mark(@mark)                         
-      if check_if_won(game_state) == @mark
-        moves_with_rating[move] = 10
-      elsif check_if_won(game_state) == opponent_mark
-        moves_with_rating[move] = -10
-      elsif check_if_drawn(game_state) == true
-        moves_with_rating[move] = 0
-      end
+     
+     ## TODO: need to incorporate depth - how many marks need to be placed before game is won? ##
+     game_state(free_positions_hash_dup, game_state)
+
+      opponent_mark = switch_mark(@mark)
+      assign_score(game_state, move, opponent_mark, moves_with_rating)                         
 
      # get the first key and the first value of the hash
      first_key = free_positions_hash.keys[0]
@@ -84,14 +61,55 @@ class PerfectPlayer
      free_positions_hash.delete(first_key)
      # create a new hash out of the array
      new_hash = {first_key => first_value}
-     #merge the key value pair into the hash - it will be the last element
+     # merge the key value pair into the hash - it will be the last element
      new_free_positions_hash = free_positions_hash.merge(new_hash)
 
      free_positions_hash_dup = new_free_positions_hash.clone
     end
-    # moves_with_rating =  {5=>-10, 6=>10, 7=>-10, 8=>-10}
     move = moves_with_rating.key(10)
+
+    # check for all moves with rating 10, if won of them wins straight away
+    if moves_with_rating.length > 1 
+      mark = @mark
+      good_moves = moves_with_rating.delete_if {|move, rating| rating != 10}
+      
+      # check if any of the good moves could be a win for 'x'
+      good_moves.each do |move, rating|
+        game_state = @board.cells.clone
+        game_state[move] = 'x' 
+        if check_if_won(game_state) != false
+          return move
+        end
+      end
+
+      # check if any of the good moves could be a win for 'x'
+      good_moves.each do |move, rating|
+        game_state = @board.cells.clone
+        game_state[move] = 'o' 
+        if check_if_won(game_state) != false
+          return move
+        end
+      end
+
+    end
     return move
+ end
+
+ def assign_score(game_state, move, opponent_mark, moves_with_rating)
+  if check_if_won(game_state) == @mark
+    moves_with_rating[move] = 10
+  elsif check_if_won(game_state) == opponent_mark
+    moves_with_rating[move] = -10
+  elsif check_if_drawn(game_state) == true
+    moves_with_rating[move] = 0
+  end
+   
+ end
+
+ def game_state(free_positions_hash_dup, game_state)
+   free_positions_hash_dup.each do |index, mark|
+    game_state[index] = mark 
+   end
  end
 
   def check_if_drawn(game_state)
@@ -107,8 +125,7 @@ class PerfectPlayer
 
   def check_if_won(game_state)
     winner = false
-    win_arrays = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-    win_arrays.each do |win_array|
+    WIN_ARRAYS.each do |win_array|
        x_counter = 0
        o_counter = 0
        win_array.each do |position|
@@ -143,14 +160,6 @@ class PerfectPlayer
     end
   end
 
-  def score
-    if @board.board_full? && @board.check_if_won == false
-      0
-    elsif @board.check_if_won == 'x' || @board.check_if_won == 'o'
-      10
-    end
-  end
-
   def corner_move
     available_corner_moves = @board.available_positions & corners
     available_corner_moves.first
@@ -159,7 +168,5 @@ class PerfectPlayer
   def corners
     [0,2,6,8]
   end
-
-
 
 end
--- spec/perfect_player_spec.rb
+++ spec/perfect_player_spec.rb
@@ -9,16 +9,6 @@ describe PerfectPlayer do
   end
   let(:perfect_player) {PerfectPlayer.new('x', @board)}
 
-  it 'returns a score of 0 if it is a draw' do
-    full_board
-    expect(perfect_player.score).to eq(0)
-  end
-
-  it 'returns a score of 10 if the game is won' do
-    vertical_win
-    expect(perfect_player.score).to eq(10)
-  end
-
   it 'places the mark in a free corner if there are less than two marks on the board' do
     @board = Board.new(['x',1,'o',
                         3,4,5,
@@ -33,6 +23,19 @@ describe PerfectPlayer do
     expect(perfect_player.mini_max).to eq(6)
   end
 
+  it 'blocks a potential win for the opponent' do
+    @board = Board.new(['x','x','o',
+                         3,  4, 'o',
+                         6,  7,  8 ])
+    expect(perfect_player.mini_max).to eq(8)
+  end
+
+  it 'makes the winning move if possible' do
+    @board = Board.new(['x','x','o',
+                        'x',  4,  5,
+                         6,  'o', 8 ])
+    expect(perfect_player.mini_max).to eq(6)
+  end
 
 
 end
