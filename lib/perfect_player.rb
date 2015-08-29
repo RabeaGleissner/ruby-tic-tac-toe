95b0480db059ccac17d602cf0b4f3ee86db8a27c
Rabea Gleissner <rabeagleissner@gmail.com>
2015-08-29 12:40:51 +0100

    computer places mark in corner if there are less than three marks on the board

--- lib/perfect_player.rb
+++ lib/perfect_player.rb
@@ -7,8 +7,22 @@ class PerfectPlayer
   end
 
   def return_move
-    1
+    if @board.board_full? == false && @board.check_if_won == false
+      if @board.available_positions.length >= 2
+        corner_move
+      else
+        6
+      end
+    else
+      puts "Game over"
+    end
   end
+
+  ### Mini-Max ###
+  # takes a copy of game state and makes first available move, switches to opponent mark, makes second available move, switches to own mark, makes move etc. - outcome of game will give score to the first move
+  # if won, score is 1, if drawn, score is 0, if lost, score is -1
+  # takes another copy of the initial game state and starts with second available move, places third available move, then switches mark, places fourth available move - need to go back to the beginning and use first available move last - outcome of game will give score to second move
+  # after all options have been played, get first move with score 1 and make it
 
   def score
     if @board.board_full? && @board.check_if_won == false
@@ -16,6 +30,15 @@ class PerfectPlayer
     elsif @board.check_if_won == true
       1
     end
+  end
+
+  def corner_move
+    available_corner_moves = @board.available_positions & corners
+    available_corner_moves.first
+  end
+
+  def corners
+    [0,2,6,8]
   end
 
 
--- spec/perfect_player_spec.rb
+++ spec/perfect_player_spec.rb
@@ -9,10 +9,6 @@ describe PerfectPlayer do
   end
   let(:perfect_player) {PerfectPlayer.new('x', @board)}
 
-  it 'chooses a position to make a move' do 
-    expect(perfect_player.return_move).to eq(1)
-  end
-
   it 'returns a score of 0 if it is a draw' do
     full_board
     expect(perfect_player.score).to eq(0)
@@ -21,6 +17,13 @@ describe PerfectPlayer do
   it 'returns a score of 1 if the game is won' do
     vertical_win
     expect(perfect_player.score).to eq(1)
+  end
+
+  it ' places the mark in a free corner if there are less than two marks on the board' do
+    @board = Board.new(['x',1,'o',
+                        3,4,5,
+                        6,7,8])
+    expect(perfect_player.return_move).to eq(6)
   end
 
 
